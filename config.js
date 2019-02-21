4c3eb1bc7c92b0e09b5f6a4c071f6d217b8a5994
cryptokatz <kahthan@gmail.com>
2019-02-21 06:19:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ var discord = "https://discordapp.com/invite/YourPool";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "https://explorer.conceal.network/index.html?hash={id}#blockchain_block";
+var transactionExplorer = "https://explorer.conceal.network/index.html?hash={id}##blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
